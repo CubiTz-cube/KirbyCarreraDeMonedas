2432d5dfec420a49f166b5ce7eca7d0eedec7570
DanielCarrenoMar <dacarreno.24@est.ucab.edu.ve>
2024-10-14 21:51:47 -0400

    Agregar Base para Testing

--- core/src/main/java/src/main/Main.java
+++ core/src/main/java/src/main/Main.java
@@ -1,16 +1,14 @@
 package src.main;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+public class Main extends Game {
+    public SpriteBatch batch;
+    public Texture image;
 
     @Override
     public void create() {
