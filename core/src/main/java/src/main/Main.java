b97f7291a90c2d47414ae875a6420bc765ae162c
juan david <jdacevedo.24@est.ucab.edu.ve>
2024-12-02 14:23:28 -0400

    Feat: Monedas no colisionan con enemigos y main empieza en intro

--- core/src/main/java/src/main/Main.java
+++ core/src/main/java/src/main/Main.java
@@ -128,7 +128,7 @@ public class Main extends Game {
         screensList.add(new MiniDuckScreen(this, (GameScreen) screensList.get(Screens.GAME.ordinal())));
         screensList.add(new OdsPleaseScreen(this, (GameScreen) screensList.get(Screens.GAME.ordinal())));
 
-        changeScreen(Screens.ODSPLEASE);
+    changeScreen(Screens.INTRO);
     }
 
     public void setName(String name) {
