5faf450194d318a8a3ab5a8b1bcebf238b393aa9
DanielCarrenoMar <dacarreno.24@est.ucab.edu.ve>
2024-12-29 11:50:46 -0400

    Feat: InfoScreen

--- core/src/main/java/src/screens/uiScreens/InfoScreen.java
+++ core/src/main/java/src/screens/uiScreens/InfoScreen.java
@@ -2,16 +2,34 @@ package src.screens.uiScreens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
 import src.main.Main;
+import src.screens.components.LayersManager;
+import src.utils.constants.MyColors;
 
 public class InfoScreen extends BlueCircleScreen {
 
     public InfoScreen(Main main) {
         super(main, "Acerca de", null, Main.Screens.MENU);
+
+        LayersManager layersManager = new LayersManager(stageUI, 3);
+
+        Label infoLabel = new Label(
+            "Controles\n" +
+            "W/A/S/D Moverse\n" +
+            "Espacio Ejecutar accion\n" +
+            "Shift Correr\n",
+            new Label.LabelStyle(fontBri, MyColors.BLUE));
+
+        layersManager.setZindex(0);
+        layersManager.getLayer().center();
+        layersManager.getLayer().pad(40);
+        layersManager.getLayer().add(infoLabel).expandX().fill().padRight(10);
 
     }
 
